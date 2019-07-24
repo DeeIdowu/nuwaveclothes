34f95dffb398d563ef1d780e503477787916b000
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-07-24 13:17:32 -0400

    menuworks

---
+++ src/components/directory/Directory.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+import "./directory.scss";
+
+import MenuItem from "../menu-item/MenuItem";
+
+//Due to state required its going to be class component simply due to the fact we must store the state of changes applied to the menu
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          id: 1,
+          size: "large",
+          linkUrl: "shop/hats"
+        },
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2,
+          size: "large",
+          linkUrl: "shop/jackets"
+        },
+        {
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          id: 3,
+          size: "large",
+          linkUrl: "shop/sneakers"
+        },
+        {
+          title: "womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: "large",
+          id: 4,
+          linkUrl: "shop/womens"
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: "large",
+          id: 5,
+          linkUrl: "shop/mens"
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className="directory-menu">
+        {//importing section itmes into menu
+        this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
