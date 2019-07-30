ccd0829401e3d571f1ed2811186a14bf0f45d27b
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-07-30 13:41:51 -0400

    authenticationxdatabase

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Homepage from "./components/pages/homepage/Homepage";
 import Shop from "./components/pages/shop/Shop";
 import SignInSignOut from "./components/pages/sign-in-sign-out/SignInSignOut";
 //import Firebase Auth:
-import { auth } from "./firebase/firebase.utils";
+import { auth, createdUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -23,10 +23,32 @@ class App extends React.Component {
 
   //to fire firebase authentication:
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    auth.onAuthStateChanged(async userAuth => {
+      //if user auth exist:
+      if (userAuth) {
+        //obtaining userRef object form the utils/data checking if database updated:
+        const userRef = await createdUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          //display new user or user already stored in database via DocumentSnapshot
+          //example to obtain properties/data of user:
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+              //for confirmation the above function is working:
+            },
+            () => console.log(this.state)
+          );
+        });
+      }
+      //for the return of user being null if users signs out
+      this.setState({ currentUser: userAuth });
     });
   }
+
   //for halting memory leaks:
   componentWillUnmount() {
     this.unsubscribeFromAuth();
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,41 @@ const config = {
   appId: "1:109010889992:web:75e1df474250c1c1"
 };
 
+//to extract the o-auth from google and placed into firebase database:
+export const createdUserProfileDocument = async (userAuth, additonalData) => {
+  //save to database when they log in not when signed out, evaluating the null return
+  if (!userAuth) return;
+
+  //if user exists, query through database/document to confirm if it exist - queryreference/query snapshot
+  //these return in a format of document or collection of from the database.
+  //determinant of rerun of data.
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  //to obtain snapshot of the user ref in the document
+  const snapShot = await userRef.get();
+
+  //if the snapshot doesnt exist do this:
+  if (!snapShot.exists) {
+    //utilizing documentRef to confirm data & id:
+    //what properties do we want to store
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    //asynchronous request to database to store user if the user doesnt exist, then set the user data below:
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additonalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  //return the userRef for future use
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
