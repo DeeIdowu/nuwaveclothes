3df9d39c550eb2851d7e84b085b80d7363026c79
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-08-06 16:17:57 -0400

    removal of cart items

--- src/redux/cart/CartActions.js
+++ src/redux/cart/CartActions.js
@@ -9,3 +9,8 @@ export const addItem = item => ({
   type: CartActionTypes.ADD_ITEM,
   payload: item //payload is refering action.payload, the value of payload is item
 });
+
+export const clearItemFromCart = item => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item
+});
