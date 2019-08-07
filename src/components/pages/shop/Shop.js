c2478971b38cc2f2f605918dccd232aad3546317
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-08-07 18:42:05 -0400

    implementing routing via collections-overview

--- src/components/pages/shop/Shop.js
+++ src/components/pages/shop/Shop.js
@@ -1,20 +1,11 @@
 import React from "react";
-import { selectCollections } from "../../../redux/shop/ShopSelector";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
 
-import PreviewCollection from "../../preview-collection/PreviewCollection";
+import CollectionsOverview from "../../collections-overview/CollectionsOverview";
 
 const Shop = ({ collections }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <PreviewCollection key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionsOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-});
-
-export default connect(mapStateToProps)(Shop);
+export default Shop;
