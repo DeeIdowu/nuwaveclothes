00c4c5265ad67347b46d6a87600ce0076e28320b
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-08-08 13:48:13 -0400

    removing logger from production build

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -4,7 +4,11 @@ import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
 //setting up middleware via array - the array enables you to add multiple middlewares
-const middlewares = [logger];
+const middlewares = [];
+//to not display logger in production:
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 //creation of store
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
