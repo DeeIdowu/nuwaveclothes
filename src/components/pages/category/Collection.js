686cbcbbc12f65beaa91b6aa858e23bfb11c7a7d
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-08-07 20:01:27 -0400

    routing via currying,match and find method

--- src/components/pages/category/Collection.js
+++ src/components/pages/category/Collection.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { connect } from "react-redux";
+import { selectCollection } from "../../../redux/shop/ShopSelector";
 import "./collection.scss";
 //to display the items but categorized
 import CollectionItem from "../../collection-item/CollectionItem";
@@ -9,5 +10,9 @@ const Collection = () => (
     <h1>Collection</h1>
   </div>
 );
+//grabbing property of component via ownProps
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
 
-export default Collection;
+export default connect(mapStateToProps)(Collection);
