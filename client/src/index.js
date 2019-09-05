b7ba271a01772b11981c86630c531f2edf329e64
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-09-05 19:20:42 -0400

    made pwa

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 //importing persistor and component for persisted reducer
 import { PersistGate } from "redux-persist/integration/react";
-
+import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import App from "./App";
 
@@ -22,3 +22,6 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+//for PWA
+serviceWorker.register();
